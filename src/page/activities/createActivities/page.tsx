82115c96b6bf95c4af4c774c71037a4df666f42e
jonnypaulino <jonnyzico10@gmail.com>
2024-10-21 10:40:19 -0300

    fix: tyoe

--- src/page/activities/createActivities/page.tsx
+++ src/page/activities/createActivities/page.tsx
@@ -1,11 +1,13 @@
 import { Form, Formik } from "formik"
 import { Button } from "primereact/button"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import ContentPage from "../../../Components/ContentPage"
 import { Column, Padding, Row } from "../../../Styles/styles"
 import Inputs from "../components/inputs"
 import { CreateActivitiesType } from "../type"
 import CreateActivitiesProvider, { CreateActivitiesContext } from "./context/context"
+import CreateOrEditForm from "../CreateForms"
+import { TabMenu } from "primereact/tabmenu"
 
 const ActivitiesCreate = () => {
     return (
@@ -17,16 +19,30 @@ const ActivitiesCreate = () => {
 
 const ActivitiesCreatePage = () => {
     const activitiesCreate = useContext(CreateActivitiesContext) as CreateActivitiesType
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [typeActivites, setTypeActivites] = useState<string | undefined>()
+
+    const items = [
+        { label: 'Atividade', icon: 'pi pi-home' },
+        // { label: 'Formulário', icon: ' pi pi-list' },
+    ];
+
+    const itemsQuiz = [
+        { label: 'Atividade', icon: 'pi pi-home' },
+        { label: 'Formulário', icon: ' pi pi-list' },
+    ];
 
 
     return (
         <ContentPage title="Criar atividades" description="">
             <Padding />
-            <Formik
+            <TabMenu model={typeActivites === "QUIZ" ? itemsQuiz :items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)} />
+            {activeIndex === 0 && <Formik
                 initialValues={activitiesCreate.initialValue}
                 onSubmit={(values) => { activitiesCreate.CreateActivities(values) }}
             >
                 {({ errors, values, touched, handleChange, setFieldValue }) => {
+                    setTypeActivites(values.type_activities.id)
                     return (
                         <Form>
                         <Column>
@@ -39,7 +55,9 @@ const ActivitiesCreatePage = () => {
                     </Form>
                     );
                 }}
-            </Formik>
+            </Formik>}
+    {activeIndex === 1 && <CreateOrEditForm />}
+
         </ContentPage>
     )
 }
